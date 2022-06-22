f55486335732b6f18a04404b21d7a334c432e6a5
Vedant <87467646+9pingg@users.noreply.github.com>
2022-06-22 18:32:52 +0530

    Delete .modules.order.cmd

--- A4/src/new_device/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/kern/project_os/2020260_Assignment04/new_device/modules.order := {   echo /home/kern/project_os/2020260_Assignment04/new_device/new_device.ko; :; } | awk '!x[$$0]++' - > /home/kern/project_os/2020260_Assignment04/new_device/modules.order
